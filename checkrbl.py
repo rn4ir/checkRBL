384499b98a1c847c2c8204b2499d1c765a1095ec
rn4ir <rn@rahul-nair.com>
2017-09-08 21:12:16 -0400

    added ip checks

--- checkrbl.py
+++ checkrbl.py
@@ -1,14 +1,38 @@
 #!/usr/bin/env python
 
+# imports
+import sys
+import ipaddress
+
 rbllist = []
+ctr = 0
+IP = ""
 
 with open("rbllist.txt", "r") as rbltext:
     for line in rbltext:
         rbllist.append(line.strip())
 rbltext.close()
 
-ctr = 0
+def validateIP(ipaddr):
+    if ipaddress.ip_address(ipaddr):
+        if ipaddress.IPv4Address(ipaddr):
+            return 1
+        elif ipaddress.IPv6Address(ipaddr):
+            return 1
+        else:
+            return 0
+    else:
+        return 0
 
-for rbl in rbllist:
-    ctr += 1
-    print (str(ctr) + " " + rbl)
+if len(sys.argv) < 2:
+    print ("Enter at least one IP")
+elif len(sys.argv) > 2:
+    print ("Enter only one IP")
+else:
+    print (sys.argv[1])
+    IP = sys.argv[1]
+    if validateIP(IP):
+        print ("checking now")
+    else:
+        print ("incorrect input")
+
