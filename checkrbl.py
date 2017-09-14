bb11c6e035e1ea6aa376a91a6a2dc84ba2b3b7bc
rn4ir <rn@rahul-nair.com>
2017-09-13 22:59:16 -0400

    added exceptions

--- checkrbl.py
+++ checkrbl.py
@@ -14,6 +14,7 @@ with open("rbllist.txt", "r") as rbltext:
 rbltext.close()
 
 def validateIP(ipaddr):
+    """
     if ipaddress.ip_address(ipaddr):
         if ipaddress.IPv4Address(ipaddr):
             return 1
@@ -23,16 +24,34 @@ def validateIP(ipaddr):
             return 0
     else:
         return 0
+    """
+    try:
+        ipaddress.ip_address(ipaddr)
+        if ipaddress.IPv4Address(ipaddr):
+           return 1
+        elif ipaddress.IPv6Address(ipaddr):
+            return 1
+        else:
+            return 0
+    except ValueError as e:
+        print (str(e.args))
+
+def networkIP (ipaddr):
+    if ipaddress.ip_address(ipaddr).is_private:
+        print ("Private IP. Exiting.." + ipaddr)
+        sys.exit()
+    else:
+        return ipaddr
 
 if len(sys.argv) < 2:
     print ("Enter at least one IP")
 elif len(sys.argv) > 2:
     print ("Enter only one IP")
 else:
-    print (sys.argv[1])
+    #print (sys.argv[1])
     IP = sys.argv[1]
     if validateIP(IP):
-        print ("checking now")
+        if networkIP(IP):
+            print (IP)
     else:
         print ("incorrect input")
-
